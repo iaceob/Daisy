76418802340587242e2269bbbc5f50c78e168e0c
iaceob <iaceob@gmail.com>
2015-06-30 21:02:20 +0800

    Merge branch 'develop' of https://github.com/canknow13/Daisy
    Conflicts:
    	js/m/main.js
    
    merge

--- js/jskit/ext/class.js
+++ js/jskit/ext/class.js
@@ -3,7 +3,7 @@
     var self = kit.fn;
 
     function hasClass(target, cls){
-        if (!target||!cls) throw '需要目标元素和样式名称';
+        if (!target||!cls) throw new TypeError('需要目标元素和样式名称');
         return !!target.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
     };
 
@@ -25,7 +25,6 @@
     /// ========
     function addClass(cls){
         self.forEach(function(target){
-            // console.log(target);
             doAddClass(target, cls);
         });
         return self;
@@ -38,17 +37,18 @@
         return self;
     };
 
-    function triggerClass(cls){
+    function toggleClass(cls){
         self.forEach(function(target){
             hasClass(target, cls) ? doRemoveClass(target, cls) : doAddClass(target, cls);
         });
+        return self;
     };
 
 
     // self.hasClass = hasClass;
     self.addClass = addClass;
     self.removeClass = removeClass;
-    self.triggerClass = triggerClass;
+    self.toggleClass = toggleClass;
 
 
 })(window, jsKit);
