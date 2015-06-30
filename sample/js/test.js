139d1dd2425d48762daaa029d282b6897af66c7f
canknow13 <3230525823@qq.com>
2015-06-30 16:33:47 +0800

    add JExtend

---
+++ sample/js/test.js
@@ -0,0 +1,19 @@
+var promise = new Promise(function(resolve, reject) {
+    /* 异步操作成功 */
+    if (true){
+        resolve(value);
+    }
+    else {
+        reject(error);
+    }
+});
+
+promise.then(function(value) {
+    // success
+}, function(value) {
+    // failure
+});
+
+window.onload=function(){
+    console.log($("body"));
+}
